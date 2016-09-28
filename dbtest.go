84c38c684ff0c0e626eeea52408cfd5a4608bf60
Patrick Galbraith <patg@patg.net>
2016-09-28 02:36:19 +0000

    Working code

--- dbtest.go
+++ dbtest.go
@@ -6,6 +6,8 @@ import (
     "encoding/json"
     "flag"
     "fmt"
+    "math/rand"
+    "time"
     "log"
     "net/http"
     "runtime"
@@ -14,10 +16,14 @@ import (
 type Message struct {
     Message string `json:"message"`
 }
+type rres struct {
+    Id int
+    Msg string
+    Time string
+}
 
 var (
     debug   = flag.Bool("debug", false, "debug logging")
-    message = flag.String("message", "Hello, World!", "message to return")
     port    = flag.Int("port", 9080, "port to serve on")
     mysql_port = flag.Int("mysql-port", 3306, "mysql db port")
     mysql_user = flag.String("mysql-user", "test", "mysql user")
@@ -26,16 +32,17 @@ var (
     mysql_db = flag.String("mysql-db", "test", "mysql schema")
 )
 
-func dbprocess() {
+func dbsetup() {
     dsn := fmt.Sprintf("%s:%s@/%s", *mysql_user, *mysql_password, *mysql_db)
     //db, err := sql.Open("mysql", "user:password@/dbname")
     db, err := sql.Open("mysql", dsn)
 
-    // insert
-    stmt, err := db.Prepare("INSERT users SET first=?,last=?,age=?,created=now()")
+    stmt, err := db.Prepare("drop table if exists t1")
+    res, err := stmt.Exec()
     checkErr(err)
 
-    res, err := stmt.Exec("first", "last", 48)
+    stmt, err = db.Prepare("create table t1 (id int auto_increment, message varchar(256) not null default '', created datetime, primary key (id)) engine=innodb")
+    res, err = stmt.Exec()
     checkErr(err)
 
     id, err := res.LastInsertId()
@@ -43,34 +50,56 @@ func dbprocess() {
 
     fmt.Println(id)
 
-    // query
-    rows, err := db.Query("SELECT * FROM users")
+}
+
+func random(min, max int) int {
+    rand.Seed(time.Now().Unix())
+    return rand.Intn(max - min) + min
+}
+
+func dbprocess(msg string) rres {
+    dsn := fmt.Sprintf("%s:%s@/%s", *mysql_user, *mysql_password, *mysql_db)
+    //db, err := sql.Open("mysql", "user:password@/dbname")
+    db, err := sql.Open("mysql", dsn)
+
+    // insert
+    stmt, err := db.Prepare("INSERT into t1 SET message=?,created=now()")
     checkErr(err)
 
-    for rows.Next() {
-        var id int
-        var age int
-        var first string
-        var last string
-        var created string
-        err = rows.Scan(&id, &first, &last, &age, &created)
-        checkErr(err)
-        fmt.Println(id)
-        fmt.Println(first)
-        fmt.Println(last)
-        fmt.Println(age)
-        fmt.Println(created)
-    }
+    _, err = stmt.Exec(msg)
+    checkErr(err)
+
+    // query
+    var min int
+    var max int
+    err = db.QueryRow("SELECT min(id), max(id) FROM t1").Scan(&min, &max)
+    checkErr(err)
+
+    rnd := random(min, max)
+    fmt.Println("random num: %d", rnd)
+
+    var id int
+    var created string
+    var message string
+    err = db.QueryRow("SELECT * FROM t1 where id = ?", rnd).Scan(&id, &message, &created)
+    checkErr(err)
+
+    resstruct := rres{id, msg, created}
+    //result_string = fmt.Sprintf("|%d|%s|%s|", id, msg, created)
+
+    //fmt.Println(resstruct)
+    //fmt.Println(recs)
 
     db.Close()
+    return resstruct
 }
 
 func main() {
     flag.Parse()
-    dbprocess()
 
     runtime.GOMAXPROCS(runtime.NumCPU())
 
+    dbsetup()
     http.HandleFunc("/json", jsonHandler)
     http.ListenAndServe(fmt.Sprintf(":%d", *port), Log(http.DefaultServeMux))
 }
@@ -86,8 +115,10 @@ func Log(handler http.Handler) http.Handler {
 
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
-    msg := r.FormValue("test")
-    json.NewEncoder(w).Encode(&Message{msg})
+    msg := r.FormValue("msg")
+    resstruct := dbprocess(msg)
+    //json.NewEncoder(w).Encode(&Message{resstruct})
+    json.NewEncoder(w).Encode(resstruct)
 }
 
 func checkErr(err error) {
